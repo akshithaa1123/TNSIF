82a0c06d148129df157b4e2ebe4b668807b5fcfb
AKSHITHA <akshitha.ananthala123@gmail.com>
2025-05-08 20:29:21 +0530

    Arrays

---
+++ tns/src/com/tnsif/arraydemo/ArrayExample.java
@@ -0,0 +1,14 @@
+package com.tnsif.arraydemo;
+
+public class ArrayExample {
+		public static void main(String[] args) {
+	        // Declaring and initializing an array of integers
+	        int[] arr = {1, 2, 3, 4, 5};
+	        // Printing array elements using a for loop
+	        System.out.println("Array elements:");
+	        for (int i = 0; i < arr.length; i++) {
+	            System.out.println(arr[i]);
+	        }
+	    }
+	}
+
