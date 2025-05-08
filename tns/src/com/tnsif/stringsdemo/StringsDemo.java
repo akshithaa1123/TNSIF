b31614dc84aca992c340a2ab3672599016596b6a
AKSHITHA <akshitha.ananthala123@gmail.com>
2025-05-08 20:00:46 +0530

    Strings

---
+++ tns/src/com/tnsif/stringsdemo/StringsDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.stringsdemo;
+
+public class StringsDemo {
+
+		public static void main(String args[]) {
+			StringBuffer S1=new StringBuffer();
+			System.out.println(S1.capacity());
+			StringBuffer S2=new StringBuffer("TNSIF");
+			System.out.println(S2.capacity());
+		}
+	}
+
